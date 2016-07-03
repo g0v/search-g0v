eb6846e53874c75c3c8f6ab5c5f460facf033c70
洪盟翔 <andyhorng168@gmail.com>
2016-07-03 12:31:50 +0800

    use YAPF to format source code

--- make_index.py
+++ make_index.py
@@ -19,33 +19,32 @@ g = git.cmd.Git('hackpad-backup-g0v')
 g.execute(["git", "submodule", "update"])
 print('pull from hackpad-backup-g0v: done')
 
-
 try:
-	last_update = open('last_update.txt').read()
-	#讀取 pads.json ++
+    last_update = open('last_update.txt').read()
+    #讀取 pads.json ++
 except:
-	last_update = 0
+    last_update = 0
 
-with open('hackpad-backup-g0v/pads.json') as data_file:    
-	data = json.load(data_file)
+with open('hackpad-backup-g0v/pads.json') as data_file:
+    data = json.load(data_file)
 
 git_ff = {}
 for pad in data:
-	git_ff[pad['padid']] = pad
+    git_ff[pad['padid']] = pad
 
 try:
-	# 讀取 last_pads.json ++
-	with open('last_pads.json') as lp: 
-		last_pads = json.load(lp)
+    # 讀取 last_pads.json ++
+    with open('last_pads.json') as lp:
+        last_pads = json.load(lp)
 except:
-	last_pads = {}
+    last_pads = {}
 
 ff = {}
-for k,v in git_ff.items():
-	if k not in last_pads:
-		ff[k] = git_ff[k]
-	if(v['last_backup_time'] > float(last_update)):
-		ff[k] = git_ff[k]
+for k, v in git_ff.items():
+    if k not in last_pads:
+        ff[k] = git_ff[k]
+    if (v['last_backup_time'] > float(last_update)):
+        ff[k] = git_ff[k]
 
 print("總共 " + str(len(data)) + " 個 pad")
 print("上次建立時間：" + time.ctime(float(last_update)))
@@ -57,8 +56,14 @@ print("===================================")
 analyzer = ChineseAnalyzer()
 
 # 创建schema, stored为True表示能够被检索
-schema = Schema(title=TEXT(stored=True), path=ID(stored=True, unique=True),
-				f=TEXT(stored=True, analyzer=analyzer), content=TEXT(stored=False, analyzer=analyzer), modified=TEXT(stored=True))
+schema = Schema(title=TEXT(stored=True),
+                path=ID(stored=True,
+                        unique=True),
+                f=TEXT(stored=True,
+                       analyzer=analyzer),
+                content=TEXT(stored=False,
+                             analyzer=analyzer),
+                modified=TEXT(stored=True))
 
 # 按照schema定义信息，增加需要建立索引的文档
 # 注意：字符串格式需要为unicode格式
@@ -67,49 +72,52 @@ schema = Schema(title=TEXT(stored=True), path=ID(stored=True, unique=True),
 indexdir = 'indexdir/'
 
 if not os.path.exists(indexdir):
-	os.mkdir(indexdir)
-	storage = FileStorage(indexdir)
-	ix = create_in(indexdir, schema)
+    os.mkdir(indexdir)
+    storage = FileStorage(indexdir)
+    ix = create_in(indexdir, schema)
 else:
-	ix = index.open_dir(indexdir)
-	# ix = create_in(indexdir, schema)
-	print("open")
+    ix = index.open_dir(indexdir)
+    # ix = create_in(indexdir, schema)
+    print("open")
 
 writer = ix.writer()
 
 seg_list = []
 i = 0
 for fk, fx in ff.items():
-	try:
-		title = html2text.html2text(fx['title'])
-		title.strip()
-		title = re.sub('[\s+]', '', title)
-	except:
-		title = "untitled"
+    try:
+        title = html2text.html2text(fx['title'])
+        title.strip()
+        title = re.sub('[\s+]', '', title)
+    except:
+        title = "untitled"
 
-	html = open('hackpad-backup-g0v/' + fk + '.html').read()
-	all_text = html2text.html2text(html)
-	all_text = all_text.strip()
-	all_text = re.sub('[\s+]', '', all_text)
+    html = open('hackpad-backup-g0v/' + fk + '.html').read()
+    all_text = html2text.html2text(html)
+    all_text = all_text.strip()
+    all_text = re.sub('[\s+]', '', all_text)
 
-	writer.update_document(title=title, path=u'/'+fk, f=fk,
-    	                content=all_text, modified=str(time.ctime(fx['last_backup_time'])))	
+    writer.update_document(title=title,
+                           path=u'/' + fk,
+                           f=fk,
+                           content=all_text,
+                           modified=str(time.ctime(fx['last_backup_time'])))
 
-	print(title + " | " + fk + " | " + str(time.ctime(fx['last_backup_time'])))
+    print(title + " | " + fk + " | " + str(time.ctime(fx['last_backup_time'])))
 
-	# i = i + 1
-	# if(i == 10):
-	# 	break
+    # i = i + 1
+    # if(i == 10):
+    # 	break
 
 writer.commit(merge=True)
 open('last_update.txt', 'w').close()
 with open('last_update.txt', 'a') as out:
-	done_time = time.time()
-	out.write(str(done_time) + '\n')
+    done_time = time.time()
+    out.write(str(done_time) + '\n')
 
 open('last_pads.json', 'w').close()
 with open('last_pads.json', 'a') as out:
-	out.write(json.dumps(git_ff))
-	out.write('\n')
+    out.write(json.dumps(git_ff))
+    out.write('\n')
 
 print("done")
--- search.py
+++ search.py
@@ -4,7 +4,6 @@ import json
 import whoosh.index as index
 from whoosh.qparser import QueryParser
 
-
 ix = index.open_dir("indexdir")
 qp = QueryParser("content", schema=ix.schema)
 
@@ -14,31 +13,32 @@ searcher = ix.searcher()
 i = 0
 
 while i == 0:
-	user_input = input("Some input please: ") # or `input("Some...` in python 3
-	q = qp.parse(user_input)
-	results = searcher.search(q, limit=None)
+    user_input = input("Some input please: "
+                       )  # or `input("Some...` in python 3
+    q = qp.parse(user_input)
+    results = searcher.search(q, limit=None)
 
-	for res in results:
-		ff = res.fields()
-		jsondoc = json.dumps(ff, ensure_ascii=False)
+    for res in results:
+        ff = res.fields()
+        jsondoc = json.dumps(ff, ensure_ascii=False)
 
-		# jsondoc.score = res.score
-		print(res)
-		print(res['f'])
-		if 'title' in res:
-			print(res['title'])
-		else:
-			print('untitled')
-		print(res.score)
-	
-	# print(res.fields())
-	# print(jsondoc)  # 打印出检索出的文档全部内容
-# # 检索出来的第一个结果，数据格式为dict{'title':.., 'content':...}
-# firstdoc = results[0].fields()
+        # jsondoc.score = res.score
+        print(res)
+        print(res['f'])
+        if 'title' in res:
+            print(res['title'])
+        else:
+            print('untitled')
+        print(res.score)
 
-# # python2中，需要使用json来打印包含unicode的dict内容
-# jsondoc = json.dumps(firstdoc, ensure_ascii=False)
+    # print(res.fields())
+    # print(jsondoc)  # 打印出检索出的文档全部内容
+    # # 检索出来的第一个结果，数据格式为dict{'title':.., 'content':...}
+    # firstdoc = results[0].fields()
 
-# print(jsondoc)  # 打印出检索出的文档全部内容
-# print(results[0].highlights("title"))  # 高亮标题中的检索词
-# print(results[0].score)  # bm25分数
+    # # python2中，需要使用json来打印包含unicode的dict内容
+    # jsondoc = json.dumps(firstdoc, ensure_ascii=False)
+
+    # print(jsondoc)  # 打印出检索出的文档全部内容
+    # print(results[0].highlights("title"))  # 高亮标题中的检索词
+    # print(results[0].score)  # bm25分数
